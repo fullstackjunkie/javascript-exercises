8022fa45b6c23f76c3d22103f43913cb3e2617c2
fullstackjunkie <dirtroadddeveloper@gmail.com>
2024-10-20 18:10:15 -0400

    Finished working another problem set for the course

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    
+    let arrs = [];
+    let elements = args.slice(1);
+  
+  
+    return args[0].filter(elem => {
+      return !elements.includes(elem) 
+    });
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if((num1 < 0 || typeof num1 !== "number" ||  !Number.isInteger(num1)) || (num2 < 0 || typeof num2 !== "number" || !Number.isInteger(num2))) return "ERROR";
+  
+    let sum = 0;
 
+    for(let i = (num1 < num2 ? num1 : num2); i <= (num1 > num2 ? num1 : num2); i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
